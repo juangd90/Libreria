bb5e9f7528e790858b8cc800d584a5e7d68b09d1
juangd90 <juan.gandulfo90@gmail.com>
2021-09-03 15:45:57 -0300

    Comenzando

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Producto,Servicio,VentaProducto,VentaServicio	
 # Register your models here.
+
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display=("nombre","precio_costo","precio_venta","cantidad")
+
+class ServicioAdmin(admin.ModelAdmin):
+    list_display=("nombre","precio_venta")
+
+class VentaProductoAdmin(admin.ModelAdmin):
+    list_display=("fecha","producto","ganancia","cantidad")
+
+class VentaServicioAdmin(admin.ModelAdmin):
+    list_display=("fecha","servicio","ganancia","cantidad")        
+
+admin.site.register(Producto,ProductoAdmin)    
+admin.site.register(Servicio,ServicioAdmin)
+admin.site.register(VentaServicio,VentaServicioAdmin)
+admin.site.register(VentaProducto,VentaProductoAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre=models.CharField(max_length=64)
+    cantidad=models.IntegerField()
+    precio_costo=models.FloatField()
+    precio_venta=models.FloatField()
+    
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Servicio(models.Model):
+    nombre=models.CharField(max_length=64)
+    precio_venta=models.FloatField()
+
+    def __str__(self):
+        return self.nombre
+
+class VentaServicio(models.Model):
+    fecha=models.DateField()
+    servicio=models.ForeignKey(Servicio,on_delete=models.CASCADE,blank=True)    
+    ganancia=models.FloatField(default=1)
+    cantidad=models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.fecha}-{self.ganancia}"
+
+class VentaProducto(models.Model):
+    fecha=models.DateField()
+    producto=models.ForeignKey(Producto,on_delete=models.CASCADE,blank=True)    
+    ganancia=models.FloatField(default=1)
+    cantidad=models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.fecha}-{self.ganancia}"
